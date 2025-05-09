a16819c6d6a351e0892681d7fbd7ca9e5f20f9b9
Park-nanasi <park.sungjin@smart-group.co.jp>
2025-05-09 14:38:17 +0900

    Issue6

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import in.tech_camp.training_curriculum_java.repository.PlanRepository;
-import in.tech_camp.training_curriculum_java.form.PlanForm;
 import in.tech_camp.training_curriculum_java.entity.PlanEntity;
-
+import in.tech_camp.training_curriculum_java.form.PlanForm;
+import in.tech_camp.training_curriculum_java.repository.PlanRepository;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -66,9 +65,15 @@ public class CalendarsController {
           }
       }
 
+      int wdayNum = currentDate.getDayOfWeek().getValue();
+      if(wdayNum >= 7){
+        wdayNum = wdayNum -7;
+      }
+
       day_map.put("month", currentDate.getMonthValue());
       day_map.put("date", currentDate.getDayOfMonth());
       day_map.put("plans", todayPlans);
+      day_map.put("wday", wdays[wdayNum]);
 
       weekDays.add(day_map);
     }
